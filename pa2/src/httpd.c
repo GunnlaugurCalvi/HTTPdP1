6261657d911d9f3d04637896625ddfc0cd2147cd
gunnlaugur15 <gunnlaugur15@skel.ru.is>
2017-10-05 19:06:56 +0000

    Date in ISO 8601 format

--- pa2/src/httpd.c
+++ pa2/src/httpd.c
@@ -12,7 +12,7 @@
 #include <stdbool.h>
 #include <glib.h>
 #include <poll.h>
-//#include <libexplain/poll.h>
+//#include <libexplain/poll.h> would be beautiful if this would work
 int main(int argc, char *argv[])
 {
 	//Initialize and declare variables
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
 	struct pollfd fd;
 	int timeout_msecs = 500;
 	char buf[1025];
-	time_t sec;
-
-
+	time_t t;
+	struct tm *timeZone = NULL;
+	
 	//Input is valid
 	if(argc != 2){
 		printf("[ERR] Invalid input: Must take only 1 parameter(portnumber) \n");
@@ -75,15 +75,20 @@ int main(int argc, char *argv[])
 			exit(EXIT_FAILURE);	
 		}
 		//Start the clock
-		sec = time(NULL);
-		
+		t = time(NULL);
+		timeZone = localtime(&t);
+	
 		//Get the client IP and port in human readable form
 		char *clientIP = inet_ntoa(client.sin_addr);
 		unsigned short cPort = ntohs(client.sin_port);
-		fprintf(stdout,"Client IP %s:%d\n", clientIP, cPort);	
+		fprintf(stdout,"Client IP and port %s:%d\n", clientIP, cPort);	
 		fflush(stdout);
-		//Write out time and filedescriptor
-		snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&sec));
+		//Write out time in ISO 8601 format!
+		//Could not use the g_time_val_to_iso8601() because it is in version 2.12
+		if(strftime(buf,sizeof(buf), "%FT%T\n", timeZone) == 0){
+			fprintf(stderr, "Strftime error\n");
+			exit(EXIT_FAILURE);
+		}
 		write(connfd, buf, strlen(buf));
 		
 		shutdown(connfd, SHUT_RDWR);
